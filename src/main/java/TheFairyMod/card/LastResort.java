72fec65ec6fdee8f576efeb221d919f8614468d7
TheGoldenFairy <lonelysniper_17@hotmail.com>
2019-10-11 16:17:25 -0500

    Bullet Amount Gained and Required Changed. Damage for Cards requiring Bullets increased.

---
+++ src/main/java/TheFairyMod/card/LastResort.java
@@ -0,0 +1,59 @@
+package TheFairyMod.card;
+
+import TheFairyMod.TheFairyMod;
+import TheFairyMod.character.TheGunner;
+import TheFairyMod.power.LastEffortPower;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static TheFairyMod.TheFairyMod.makeCardPath;
+
+public class LastResort extends AbstractFairyCard {
+
+    //card Info
+    public static final String ID = TheFairyMod.makeID("LastResort");
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String IMG = makeCardPath("Skill.png");
+    private static final String NAME = cardStrings.NAME;
+    private static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    private static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+
+
+    //card Stats
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    private static final CardColor COLOR = TheGunner.Enums.COLOR_BROWN;
+
+    //card Number
+    private static final int COST = 2;
+    private static final int POWER_AMT = 1;
+
+    //card Initialize
+    public LastResort() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = POWER_AMT;
+        exhaust = true;
+        isEthereal = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new LastEffortPower(p, magicNumber)));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            isEthereal = false;
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
